f7c07008e9fe94f2666964110868cbf588aaaf4d
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-08 21:00:55 +0300

    Добавлен контроллер отвечающий за физику падения у противников

---
+++ Assets/Scripts/Core/Enemy/RagdollKinematicController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace HitMaster.Core.Enemy {
+	public class RagdollKinematicController : MonoBehaviour {
+		[SerializeField] private Animator _animator;
+		[SerializeField] private Rigidbody[] _rigidbodies;
+
+		private void Awake() {
+			SetRigidbodyKinematic(true);
+		}
+
+		[ContextMenu("SetPhysical")]
+		public void SetPhysical() {
+			_animator.enabled = false;
+			SetRigidbodyKinematic(false);
+		}
+
+		private void SetRigidbodyKinematic(bool isKinematic) {
+			foreach (var currentRigidbody in _rigidbodies) {
+				currentRigidbody.isKinematic = isKinematic;
+			}
+		}
+	}
+}
