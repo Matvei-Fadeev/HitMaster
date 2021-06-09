47f6ffaa36e5ebc5d3e0d58d6ff6fe010284bdcc
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-09 16:32:02 +0300

    Add: navMeshAgent controller

---
+++ Assets/Scripts/Core/Player/MovementToWaypoints.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace HitMaster.Core.Player {
+	public class MovementToWaypoints : MonoBehaviour {
+		[SerializeField] private Transform[] points;
+
+		private NavMeshAgent _navMeshAgent;
+		private int _currentPoint;
+
+		private void Awake() {
+			_navMeshAgent = GetComponent<NavMeshAgent>();
+		}
+
+		[ContextMenu(nameof(GoToNextPoint))]
+		private void GoToNextPoint() {
+			if (_currentPoint < points.Length) {
+				GoToPoint(points[_currentPoint].position);
+				_currentPoint++;
+			}
+			else {
+				Debug.LogWarning("Object at the last point");
+			}
+		}
+
+		private void GoToPoint(Vector3 point) {
+			_navMeshAgent.SetDestination(point);
+		}
+	}
+}
