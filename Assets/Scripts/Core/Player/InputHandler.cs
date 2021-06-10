4e34b74180a0daf90617b4ef229c0724b4a7188a
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-10 13:06:45 +0300

    Создана заглушка для InputHandler

---
+++ Assets/Scripts/Core/Player/InputHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace HitMaster.Core.Player {
+	public class InputHandler : MonoBehaviour {
+		public Vector2 GetTouchPosition() {
+			// TODO Remove
+			if (Input.GetButtonDown("Fire1")) {
+				return Vector2.one;
+			}
+			
+			if (Input.touchCount > 0) {
+				return Input.GetTouch(0).position;
+			}
+			
+			return Vector2.zero;
+		}
+		
+		public bool HasInput() {
+			return GetTouchPosition() != Vector2.zero;
+		}
+	}
+}
