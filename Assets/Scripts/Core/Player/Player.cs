6b2d49fd5ed60e3567fbf3f4df242c4423ca247b
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-10 13:06:26 +0300

    Создан класс содержащий ссылки на компоненты игрока

---
+++ Assets/Scripts/Core/Player/Player.cs
@@ -0,0 +1,16 @@
+﻿using HitMaster.Core.Enemy;
+using UnityEngine;
+
+namespace HitMaster.Core.Player {
+	public class Player : MonoBehaviour {
+		[SerializeField] private PlayerAnimationStateController _playerAnimationStateController;
+		[SerializeField] private EnemyFinder _enemyFinder;
+		[SerializeField] private MovementToWaypoints _movementToWaypoints;
+		[SerializeField] private InputHandler _inputHandler;
+		
+		public EnemyFinder EnemyFinder => _enemyFinder;
+		public PlayerAnimationStateController AnimationStateController => _playerAnimationStateController;
+		public MovementToWaypoints MovementToWaypoints => _movementToWaypoints;
+		public InputHandler InputHandler => _inputHandler;
+	}
+}
