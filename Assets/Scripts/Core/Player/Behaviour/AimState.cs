9bad0cc82e4b7d8c699a0475101581ab58e7d193
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-10 13:09:03 +0300

    На основе паттерна State реализовано поведение игрока

---
+++ Assets/Scripts/Core/Player/Behaviour/AimState.cs
@@ -0,0 +1,18 @@
+﻿namespace HitMaster.Core.Player.Behaviour {
+	internal class AimState : PlayerState {
+		public override void Start() {
+			BehaviourSystem.Player.AnimationStateController.IsAim = true;
+			BehaviourSystem.Player.MovementToWaypoints.Stop();
+		}
+
+		public override void Update() {
+			if (BehaviourSystem.Player.InputHandler.HasInput()) {
+				BehaviourSystem.SetState(new ShootingState());
+			}
+			if (!BehaviourSystem.Player.EnemyFinder.HasEnemyInRadius()) {
+				BehaviourSystem.Player.AnimationStateController.IsAim = false;
+				BehaviourSystem.SetState(new RunningState());
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Core/Player/Behaviour/PlayerBehaviourSystem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace HitMaster.Core.Player.Behaviour {
+	public class PlayerBehaviourSystem : MonoBehaviour {
+		[SerializeField] private Player player;
+		private PlayerState _currentPlayerState;
+		
+		public Player Player => player;
+
+		private void Start() {
+			SetState(new IdleState());
+		}
+
+		private void Update() {
+			_currentPlayerState.Update();
+		}
+
+		public void SetState(PlayerState playerState) {
+			if (_currentPlayerState != null) {
+				_currentPlayerState.End();
+			}
+			_currentPlayerState = playerState;
+			playerState.BehaviourSystem = this;
+			playerState.Start();
+		}
+	}
+}
