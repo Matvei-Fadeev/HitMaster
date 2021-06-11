70b4426aed9c6e58e7c1eedec3376212fd231d8a
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-11 09:05:18 +0300

    Переименован в TouchInputHandler

---
+++ Assets/Scripts/Input/TouchInputHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace HitMaster.Input {
+	public static class TouchInputHandler {
+		public static Vector2 GetTouchPosition() {
+			if (HasTouch()) {
+				return UnityEngine.Input.GetTouch(0).position;
+			}
+
+			return Vector2.zero;
+		}
+
+		public static bool HasTouch() {
+			return UnityEngine.Input.touchCount > 0;
+		}
+	}
+}
