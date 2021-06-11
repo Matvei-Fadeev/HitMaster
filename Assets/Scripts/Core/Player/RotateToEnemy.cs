a28445af6321d323cbc8e6803a8f27a8083247da
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-11 09:17:26 +0300

    Добавлен скрипт, поворачивающий в сторону одного из противников

---
+++ Assets/Scripts/Core/Player/RotateToEnemy.cs
@@ -0,0 +1,22 @@
+﻿using DG.Tweening;
+using HitMaster.Core.Enemy;
+using UnityEngine;
+
+namespace HitMaster.Core.Player {
+	public class RotateToEnemy : MonoBehaviour {
+		[SerializeField] private EnemyFinder enemyFinder;
+		[SerializeField] private Vector3 offsetFromEnemy;
+		[SerializeField] private float rotationDuration;
+		
+		private Transform _closestEnemy;	
+
+		private void FixedUpdate() {
+			if (enemyFinder.HasEnemyInRadius()) {
+				if (!_closestEnemy || _closestEnemy.GetHashCode() != enemyFinder.ClosestEnemy.GetHashCode()) {
+					_closestEnemy = enemyFinder.ClosestEnemy;
+					transform.DOLookAt(_closestEnemy.position + offsetFromEnemy, rotationDuration);
+				}
+			}
+		}
+	}
+}
