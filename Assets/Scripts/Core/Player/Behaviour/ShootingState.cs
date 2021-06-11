5972ba36a455a5de9ef304ae67e449bd3353d737
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-11 09:18:56 +0300

    Исправлена логика в AimState для правильного перехода в ShootingState

--- Assets/Scripts/Core/Player/Behaviour/ShootingState.cs
+++ Assets/Scripts/Core/Player/Behaviour/ShootingState.cs
@@ -1,11 +1,13 @@
-﻿namespace HitMaster.Core.Player.Behaviour {
+﻿using HitMaster.Input;
+
+namespace HitMaster.Core.Player.Behaviour {
 	internal class ShootingState : PlayerState {
 		public override void Start() {
 			BehaviourSystem.Player.AnimationStateController.Shooting();
 		}
 
 		public override void Update() {
-			if (!BehaviourSystem.Player.InputHandler.HasInput()) {
+			if (!TouchInputHandler.HasTouch()) {
 				BehaviourSystem.SetState(new AimState());
 			}
 		}
