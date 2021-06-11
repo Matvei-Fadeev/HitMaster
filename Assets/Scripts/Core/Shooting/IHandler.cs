c84dcbafeebd86cf62acc768309463984129ea05
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-11 09:16:14 +0300

    Добавлена система из цепочки ответственности проверки возможности произвести выстрел

---
+++ Assets/Scripts/Core/Shooting/IHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace HitMaster.Core.Shooting {
+	public interface IHandler {
+		IHandler SetNext(IHandler handler);
+
+		bool Handle(ShootingData data);
+	}
+}
---
+++ Assets/Scripts/Core/Shooting/ShotPositionNotZero.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace HitMaster.Core.Shooting {
+	class ShotPositionNotZero : ShotHandler {
+		public override bool Handle(ShootingData data) {
+			if (data.endPosition == Vector3.zero) {
+				return false;
+			}
+
+			return base.Handle(data);
+		}
+	}
+}
