0b18cdcc7284f293498216a5d2b0d4406956e4de
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-11 09:21:14 +0300

    Обновлён Player script - доваблена ссылка на оружие игрока

--- Assets/Scripts/Core/Player/Player.cs
+++ Assets/Scripts/Core/Player/Player.cs
@@ -6,11 +6,11 @@ namespace HitMaster.Core.Player {
 		[SerializeField] private PlayerAnimationStateController _playerAnimationStateController;
 		[SerializeField] private EnemyFinder _enemyFinder;
 		[SerializeField] private MovementToWaypoints _movementToWaypoints;
-		[SerializeField] private InputHandler _inputHandler;
+		[SerializeField] private Weapon.Weapon _weapon;
 		
 		public EnemyFinder EnemyFinder => _enemyFinder;
 		public PlayerAnimationStateController AnimationStateController => _playerAnimationStateController;
 		public MovementToWaypoints MovementToWaypoints => _movementToWaypoints;
-		public InputHandler InputHandler => _inputHandler;
+		public Weapon.Weapon Weapon => _weapon;
 	}
 }
